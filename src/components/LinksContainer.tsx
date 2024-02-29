117fa70633e631e35afc85453d967747d0c6e500
Gabriel Thome <eugabrielthome@gmail.com>
2024-02-29 17:42:11 -0300

    update domain links

--- src/components/LinksContainer.tsx
+++ src/components/LinksContainer.tsx
@@ -4,8 +4,8 @@ import './LinksContainer.css';
 function LinksContainer() {
   return (
     <div className='container'>
-      <a href="http://gabriel-thome.vercel.app">Blockchain Development</a>
-      <a href="http://gabriel-thome.vercel.app/digital-solutions">Desenvolvimento de Sites</a>
+      <a href="http://www.gabrielt.xyz">Blockchain Development</a>
+      <a href="http://www.gabrielt.xyz/digital-solutions">Desenvolvimento de Sites</a>
       <a href="https://bitcoin-price-tracker-v1.vercel.app/">Bitcoin Price Tracker App</a>
     </div>
   )
